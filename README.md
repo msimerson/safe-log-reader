2187bf6a99c5c6cb9d8afa22af2f4935459e5212
Matt Simerson <matt@tnpi.net>
2015-07-22 10:28:29 -0700

    liner: added options for flexible parsing
    * liner: add options with defaults
    * liner: use StringDecoder vs toString (better utf8 support)
    * liner: assign _transform and _flush as prototypes
    * liner: rename as line-splitter
    * reader: test for concurrent lines
    * test/reader: use path.join (windows support)

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Safe Log Reader
 
 ## Features
 
-[x] * Read plain text files
-[x] * Read gz compressed files (zlib)
-[ ] * Read bz2 compressed files (compressjs)
-[x] * Can stream multiple files simultaneously
-[x] Emits data as lines, upon request (paused mode)
-[ ] * Remembers lines previously read successfully and does not repeat them
-[ ] * 
-[ ] * 
+[x] - Read plain text files
+[x] - Read gz compressed files (zlib)
+[ ] - Read bz2 compressed files (compressjs)
+[x] - Can stream multiple files simultaneously
+[x] - Emits data as lines, upon request (paused mode)
+[x] - handles utf-8 multibyte characters correctly
+[ ] - Remembers lines previously read successfully and does not repeat them
+[ ] - 
+[ ] - 
--- test/reader.js
+++ test/reader.js
@@ -1,12 +1,15 @@
 
 var assert  = require('assert');
+var path    = require('path');
 
 var reader  = require('../lib/reader');
+
+var dataDir = path.join('test', 'data');
 var logLine = 'The rain in spain falls mainly on the plain.';
 
 describe('reader', function() {
     it('reads a text file', function (done) {
-        reader.createReader('./test/data/test.log')
+        reader.createReader(path.join(dataDir, 'test.log'))
         .on('read', function (data) {
             assert.equal(data, logLine);
             done();
@@ -15,21 +18,35 @@ describe('reader', function() {
     });
 
     it('reads a text file', function (done) {
-        reader.createReader('./test/data/test.log.1')
-        .on('read', function (data) {
+        var linesSeen = 0;
+        reader.createReader(path.join(dataDir, 'test.log.1'))
+        .on('read', function (data, lines, bytes) {
             assert.equal(data, logLine);
-            done();
+            linesSeen++;
+            if (linesSeen === 3) done();
         })
         .on('readable', function () { this.read(); });
     });
 
     it('reads a gzipped file', function (done) {
-        reader.createReader('./test/data/test.log.1.gz')
+        reader.createReader(path.join(dataDir, 'test.log.1.gz'))
         .on('read', function (data) {
             // console.log(data);
             assert.equal(data, logLine);
             done();
         })
         .on('readable', function () { this.read(); });
+    });
+});
+
+describe('reader', function () {
+    it.skip('reads lined appended after EOF', function (done) {
+        reader.createReader('./test/data/test.log')
+        .on('read', function (data) {
+            // body...
+        })
+        .on('readable', function () {
+            console.log('is readable');
+        });
     });
 });
