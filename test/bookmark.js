163b08675dc56de36b26312d723c00ff06f806b3
Matt Simerson <matt@tnpi.net>
2015-07-22 22:45:46 -0700

    create test bookmark dir

--- test/bookmark.js
+++ test/bookmark.js
@@ -1,5 +1,6 @@
 
 var assert  = require('assert');
+var fs      = require('fs');
 var path    = require('path');
 
 var bookDir = path.resolve('test','.bookmarks');
@@ -7,12 +8,29 @@ var book    = require('../lib/bookmark')(bookDir);
 
 describe('bookmark', function () {
 
-	var inode = "34454366";
+	var inode = '34454366';
 	var bm = {
-		"file":"/Users/matt/Documents/git/efolder/safe-log-reader/mail.log",
-		"bytes":8193,
-		"lines":45
+		'file':'/Users/matt/Documents/git/efolder/safe-log-reader/mail.log',
+		'bytes':8193,
+		'lines':45
 	};
+
+    before('creates bookmark dir', function (done) {
+        fs.mkdir(bookDir, function (err) {
+            // ignore any 'already exists' error
+            done();
+        });
+    });
+
+    after('cleans bookmark dir', function (done) {
+        fs.readdir(bookDir, function (err, files) {
+            if (err) return console.error(err);
+            for (var i=0; i<files.length; i++) {
+                fs.unlink(path.resolve(bookDir, files[i]));
+            }
+            done();
+        });
+    });
 
 	it('saves a bookmark', function (done) {
 		book.save(inode, bm, function (err) {
