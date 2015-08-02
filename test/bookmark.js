a057edf05a23393ce754da59e24029aa04c2dae4
Matt Simerson <matt@tnpi.net>
2015-08-02 14:09:02 -0700

    add test coverage for bookmark dir creation

--- test/bookmark.js
+++ test/bookmark.js
@@ -10,6 +10,19 @@ describe('bookmark', function () {
 
 	var testFile = path.resolve('./test', 'data', 'test.log');
 
+	it('creates missing bookmark dir', function (done) {
+
+		var missingDir = path.resolve('test', 'nonexistent');
+		var book = require('../lib/bookmark')(missingDir);
+		book.createDir(function () {
+			fs.stat(missingDir, function (err, stat) {
+				assert.ifError(err);
+				assert.ok(stat.isDirectory());
+				fs.rmdir(missingDir, done);
+			});
+		});
+	});
+
 	it('saves a bookmark', function (done) {
 		book.save(testFile, 45, function (err) {
 			assert.ifError(err);
