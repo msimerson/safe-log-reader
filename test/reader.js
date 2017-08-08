5d1f37205fcfce4356c49d3cba3e9528b74a0125
Matt Simerson <matt@tnpi.net>
2017-08-08 14:37:56 -0700

    eradicate grunt, depend on eslint

--- test/reader.js
+++ test/reader.js
@@ -31,11 +31,11 @@ describe('reader', function () {
 
     // console.log(arguments);
     reader.createReader(filePath, noBmReadOpts)
-    .on('readable', function () { this.readLine(); })
-    .on('read', function (data) {
-      assert.equal(data, logLine);
-      done();
-    });
+      .on('readable', function () { this.readLine(); })
+      .on('read', function (data) {
+        assert.equal(data, logLine);
+        done();
+      });
   });
 
   it('reads another text file concurrently', function (done) {
@@ -43,11 +43,11 @@ describe('reader', function () {
     var filePath = path.join(dataDir, 'test.log.1');
 
     reader.createReader(filePath, noBmReadOpts)
-    .on('read', function (data, lines, bytes) {
-      linesSeen++;
-      assert.equal(data, logLine);
-      if (linesSeen === 3) done();
-    });
+      .on('read', function (data, lines, bytes) {
+        linesSeen++;
+        assert.equal(data, logLine);
+        if (linesSeen === 3) done();
+      });
   });
 
   it('reads batches of lines', function (done) {
@@ -58,14 +58,14 @@ describe('reader', function () {
     batchOpts.noBookmark = true;
 
     reader.createReader(filePath, batchOpts)
-    .on('read', function (data, lines, bytes) {
-      linesSeen++;
-      assert.equal(data, logLine);
-      if (linesSeen === 9) done();
-    })
-    .on('drain', function (done) {
-      done(null, 0);
-    });
+      .on('read', function (data, lines, bytes) {
+        linesSeen++;
+        assert.equal(data, logLine);
+        if (linesSeen === 9) done();
+      })
+      .on('drain', function (done) {
+        done(null, 0);
+      });
   });
 
   it('maintains an accurate line counter', function (done) {
@@ -73,29 +73,29 @@ describe('reader', function () {
     var filePath = path.join(dataDir, 'test.log.1');
 
     reader.createReader(filePath, noBmReadOpts)
-    .on('read', function (data, lines, bytes) {
-      linesSeen++;
-      assert.equal(lines, linesSeen);
-      if (linesSeen === 3) done();
-    });
+      .on('read', function (data, lines, bytes) {
+        linesSeen++;
+        assert.equal(lines, linesSeen);
+        if (linesSeen === 3) done();
+      });
   });
 
   it('reads a gzipped file', function (done) {
     reader.createReader(path.join(dataDir, 'test.log.1.gz'), noBmReadOpts)
-    .on('read', function (data) {
-      // console.log(data);
-      assert.equal(data, logLine);
-      done();
-    });
+      .on('read', function (data) {
+        // console.log(data);
+        assert.equal(data, logLine);
+        done();
+      });
   });
 
   it.skip('reads a bzip2 compressed file', function (done) {
     reader.createReader(path.join(dataDir, 'test.log.1.bz2'), noBmReadOpts)
-    .on('read', function (data) {
-      // console.log(data);
-      assert.equal(data, logLine);
-      done();
-    });
+      .on('read', function (data) {
+        // console.log(data);
+        assert.equal(data, logLine);
+        done();
+      });
   });
 
   context('growing file', function () {
@@ -128,32 +128,32 @@ describe('reader', function () {
       };
 
       reader.createReader(appendFile, readerOpts)
-      .on('read', function (data, linesRead) {
-        // console.log('line: ' + linesRead + ', ' + data);
-        if (appendDone) {
-          appendsRead++;
-          tryDone();
-        }
-      })
-      .on('end', function () {
+        .on('read', function (data, linesRead) {
+          // console.log('line: ' + linesRead + ', ' + data);
+          if (appendDone) {
+            appendsRead++;
+            tryDone();
+          }
+        })
+        .on('end', function () {
 
-        if (appendCalled) return;
-        appendCalled = true;
+          if (appendCalled) return;
+          appendCalled = true;
 
-        // append in a separate process, so this one gets the event
-        child.fork(
-          path.join('test','helpers','fileAppend.js'),
-          {
-            env: {
-              FILE_PATH: appendFile,
-              LOG_LINE: (logLine + '\n'),
-            } }
-          )
-          .on('message', function (msg) {
-            // console.log(msg);
-            appendDone = true;
-          });
-      });
+          // append in a separate process, so this one gets the event
+          child.fork(
+            path.join('test','helpers','fileAppend.js'),
+            {
+              env: {
+                FILE_PATH: appendFile,
+                LOG_LINE: (logLine + '\n'),
+              }
+            })
+            .on('message', function (msg) {
+              // console.log(msg);
+              appendDone = true;
+            });
+        });
     });
   });
 
@@ -173,8 +173,8 @@ describe('reader', function () {
             env: {
               FILE_PATH: rotateLog,
               LOG_LINE: logLine + '\n',
-            } }
-          )
+            }
+          })
           .on('message', function (msg) {
             // console.log(msg);
             appendDone = true;
@@ -189,29 +189,29 @@ describe('reader', function () {
       newFile(rotateLog, logLine + '\n', function () {
 
         reader.createReader(rotateLog, readerOpts)
-        .on('read', function (data, lineCount) {
-          // console.log(lineCount + '. ' + data);
+          .on('read', function (data, lineCount) {
+            // console.log(lineCount + '. ' + data);
 
-          if (appendDone) tryDone();
-          if (renameCalled) return;
-          renameCalled = true;
+            if (appendDone) tryDone();
+            if (renameCalled) return;
+            renameCalled = true;
 
-          child.fork(
-            path.join('test','helpers','fileRename.js'),
-            {
-              env: {
-                OLD_PATH: rotateLog,
-                NEW_PATH: rotateLog + '.1',
-              } }
-            )
-            .on('message', function (msg) {
-              // console.log(msg);
-              doAppend();
-            });
-        })
-        .on('end', function () {
-        // console.log('end');
-        });
+            child.fork(
+              path.join('test','helpers','fileRename.js'),
+              {
+                env: {
+                  OLD_PATH: rotateLog,
+                  NEW_PATH: rotateLog + '.1',
+                }
+              })
+              .on('message', function (msg) {
+                // console.log(msg);
+                doAppend();
+              });
+          })
+          .on('end', function () {
+          // console.log('end');
+          });
       });
     });
 
@@ -223,44 +223,42 @@ describe('reader', function () {
       fs.writeFile(rotateLog, logLine + '\n', function () {
 
         reader.createReader(rotateLog, noBmReadOpts)
-        .on('read', function (data, lineCount) {
-          // console.log(lineCount + '. ' + data);
+          .on('read', function (data, lineCount) {
+            // console.log(lineCount + '. ' + data);
 
-          var tryDone = function () {
-            if (appendsSeen) return done();
-            setTimeout(function () { tryDone(); }, 10);
-          };
+            var tryDone = function () {
+              if (appendsSeen) return done();
+              setTimeout(function () { tryDone(); }, 10);
+            };
 
-          if (lineCount === 2) tryDone();
-          if (isRotated) return;
+            if (lineCount === 2) tryDone();
+            if (isRotated) return;
 
-          child.fork(
-            path.join('test','helpers','fileRename.js'),
-            {
-              env: {
-                OLD_PATH: rotateLog,
-                NEW_PATH: rotateLog + '.1',
-              }
-            }
-          )
-          .on('message', function (msg) {
-            // console.log(msg);
-            isRotated = true;
             child.fork(
-              path.join('test','helpers','fileAppend.js'),
+              path.join('test','helpers','fileRename.js'),
               {
                 env: {
-                  FILE_PATH: rotateLog + '.1',
-                  LOG_LINE: logLine + '\n',
+                  OLD_PATH: rotateLog,
+                  NEW_PATH: rotateLog + '.1',
                 }
-              }
-            )
-            .on('message', function (msg) {
-              // console.log(msg);
-              appendsSeen++;
-            });
+              })
+              .on('message', function (msg) {
+                // console.log(msg);
+                isRotated = true;
+                child.fork(
+                  path.join('test','helpers','fileAppend.js'),
+                  {
+                    env: {
+                      FILE_PATH: rotateLog + '.1',
+                      LOG_LINE: logLine + '\n',
+                    }
+                  })
+                  .on('message', function (msg) {
+                    // console.log(msg);
+                    appendsSeen++;
+                  });
+              });
           });
-        });
       });
     });
   });
@@ -291,11 +289,11 @@ describe('reader', function () {
       };
 
       reader.createReader(missingFile, noBmReadOpts)
-          .on('irrelevantFile', function (filename) {
-            // console.log('irrelevantFile: ' + filename);
-            assert.equal(filename, path.basename(irrelevantFile));
-            tryDone();
-          });
+        .on('irrelevantFile', function (filename) {
+          // console.log('irrelevantFile: ' + filename);
+          assert.equal(filename, path.basename(irrelevantFile));
+          tryDone();
+        });
 
       process.nextTick(function () {
         child.fork(
@@ -305,12 +303,11 @@ describe('reader', function () {
               FILE_PATH: irrelevantFile,
               LOG_LINE: (logLine + '\n'),
             }
-          }
-        )
-        .on('message', function (msg) {
-          appendDone = true;
-          // console.log('fileAppend message: ' + msg);
-        });
+          })
+          .on('message', function (msg) {
+            appendDone = true;
+            // console.log('fileAppend message: ' + msg);
+          });
       });
     });
 
@@ -323,23 +320,23 @@ describe('reader', function () {
       };
 
       reader.createReader(missingFile, noBmReadOpts)
-      .on('read', function (data) {
-        assert.equal(data, logLine);
-        tryDone();
-      })
-      .on('error', function (err) {
-        console.error('error: ' + err);
-      });
+        .on('read', function (data) {
+          assert.equal(data, logLine);
+          tryDone();
+        })
+        .on('error', function (err) {
+          console.error('error: ' + err);
+        });
 
       process.nextTick(function () {
         child.fork(
           path.join('test','helpers','fileAppend.js'),
           childOpts
         )
-        .on('message', function (msg) {
-          appendDone = true;
-          // console.log('fileAppend message: ' + msg);
-        });
+          .on('message', function (msg) {
+            appendDone = true;
+            // console.log('fileAppend message: ' + msg);
+          });
       });
     });
 
@@ -360,20 +357,20 @@ describe('reader', function () {
       }, 100);
 
       reader.createReader(filePath, readerOpts)
-      .on('readable', function () { assert.ok(false); })
-      .on('read', function (data) {
-        assert.equal(data, false);
-      });
+        .on('readable', function () { assert.ok(false); })
+        .on('read', function (data) {
+          assert.equal(data, false);
+        });
     });
 
     it('does not watch', function (done) {
       var filePath = path.join(dataDir, 'test-no-perm.log');
 
       var r = reader.createReader(filePath, readerOpts)
-      .on('readable', function () { assert.ok(false); })
-      .on('read', function (data) {
-        assert.equal(data, false);
-      });
+        .on('readable', function () { assert.ok(false); })
+        .on('read', function (data) {
+          assert.equal(data, false);
+        });
 
       process.nextTick(function () {
         assert.equal(r.watcher, undefined);
@@ -394,18 +391,18 @@ describe('reader', function () {
     var filePath = path.join(dataDir, 'test.log');
 
     var r = reader.createReader(filePath, noBmReadOpts)
-    .on('testSetup', function () {
-      r.filePath = './non-existent';
-      this.batch.limit = 5;
-      this.batch.count = 5;  // skip to batchLimit
-    })
-    .on('read', function (data) {
-      assert.equal(data, undefined);
-    })
-    .on('drain', function (cb) {
-      cb();
-      done();
-    });
+      .on('testSetup', function () {
+        r.filePath = './non-existent';
+        this.batch.limit = 5;
+        this.batch.count = 5;  // skip to batchLimit
+      })
+      .on('read', function (data) {
+        assert.equal(data, undefined);
+      })
+      .on('drain', function (cb) {
+        cb();
+        done();
+      });
   });
 
   describe('on a file previously read', function () {
@@ -428,17 +425,17 @@ describe('reader', function () {
               if (err) return done(err);
               var readLines = 0;
               reader.createReader(filePath, readerOpts)
-              .on('read', function (data) {
-                readLines++;
-                // console.log(data);
-              })
-              .on('drain', function (cb) {
-                cb();
-              })
-              .on('end', function (cb) {
-                assert.equal(readLines, 10);
-                done();
-              });
+                .on('read', function (data) {
+                  readLines++;
+                  // console.log(data);
+                })
+                .on('drain', function (cb) {
+                  cb();
+                })
+                .on('end', function (cb) {
+                  assert.equal(readLines, 10);
+                  done();
+                });
             });
           });
         });
