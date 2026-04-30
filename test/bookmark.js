f846d0148b05bab78ff3721da8c074ba8fbd7ea4
Matt Simerson <matt@tnpi.net>
2026-04-29 17:50:03 -0700

    test runner is now node:test

--- test/bookmark.js
+++ test/bookmark.js
@@ -3,6 +3,7 @@
 const assert  = require('assert');
 const fs      = require('fs');
 const path    = require('path');
+const { describe, it } = require('node:test');
 
 const bookDir = path.resolve('test','.bookmarks');
 const book    = require('../lib/bookmark')(bookDir);
@@ -11,7 +12,7 @@ describe('bookmark', function () {
 
   const testFile = path.resolve('./test', 'data', 'test.log');
 
-  it('creates missing bookmark dir', function (done) {
+  it('creates missing bookmark dir', function (_t, done) {
 
     const missingDir = path.resolve('test', 'missing-dir');
     const book = require('../lib/bookmark')(missingDir);
@@ -24,14 +25,14 @@ describe('bookmark', function () {
     })
   })
 
-  it('saves a bookmark', function (done) {
+  it('saves a bookmark', function (_t, done) {
     book.save({ file: testFile, lines: 45 }, (err) => {
       assert.ifError(err);
       done();
     })
   })
 
-  it('reads a bookmark', function (done) {
+  it('reads a bookmark', function (_t, done) {
     book.read(testFile, (err, mark) => {
       assert.ifError(err);
       assert.equal(mark.lines, 45);
@@ -39,7 +40,7 @@ describe('bookmark', function () {
     })
   })
 
-  it('errors on unwritable bookmark dir', function (done) {
+  it('errors on unwritable bookmark dir', function (_t, done) {
 
     const noPermDir = path.resolve('test', 'data', 'nowrite', 'newdir-' + Date.now());
     const book = require('../lib/bookmark')(noPermDir);
@@ -51,10 +52,9 @@ describe('bookmark', function () {
     })
   })
 
-  it('errs on unreadable bookmark', function (done) {
+  it('errs on unreadable bookmark', function (_t, done) {
     const noReadDir = path.resolve('test', 'data', 'noread');
     const book = require('../lib/bookmark')(noReadDir);
-    // console.log(book);
     book.read(path.resolve(noReadDir, 'file'), (err, mark) => {
       assert.ok(err);
       done();
@@ -66,8 +66,7 @@ describe('error handling', function () {
 
   const testFile = path.resolve('./test', 'data', 'write-error.log');
 
-  it('save emits debugging on stat error', function (done) {
-    this.timeout(4000);
+  it('save emits debugging on stat error', { timeout: 4000 }, function (_t, done) {
     process.env.MOCK_STAT_ERROR=true;
     book.save({ file: testFile, lines: 45 }, (err) => {
       process.env.MOCK_STAT_ERROR=false;
--- test/line-splitter.js
+++ test/line-splitter.js
@@ -3,16 +3,17 @@
 const assert = require('assert')
 const stream = require('stream')
 
+const { describe, it } = require('node:test');
 const LineSplitter = require('../lib/line-splitter')
 
 describe('line-splitter', () => {
-  it('creates a stream.Transform instance', function (done) {
+  it('creates a stream.Transform instance', function (_t, done) {
     const liner = new LineSplitter();
     assert.ok(liner instanceof stream.Transform);
     done();
   })
 
-  it('sets a streams encoding', function (done) {
+  it('sets a streams encoding', function (_t, done) {
     // TODO
     done();
   })
